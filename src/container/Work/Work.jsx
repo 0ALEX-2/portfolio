81ad34c4b6dfa93adb199ffc115a1adc9fe07d3a
0ALEX-2 <sakilahmed965@gmail.com>
2023-04-02 12:06:55 +0530

    project added

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,12 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ProjectCard from '../../components/projectCard/ProjectCard'
 import {motion} from "framer-motion"
 
-const Work = () => {
 
-
-
-const data=[
+const initialData=[
   {
     id:1,
     type:"collab",
@@ -53,15 +50,63 @@ const data=[
       "https://seeklogo.com/images/R/react-logo-7B3CE81517-seeklogo.com.png",
     ]
   },
+  {
+    id:4,
+    type:"single",
+    img:"https://images.ctfassets.net/lzny33ho1g45/O6Ns6DttUzJym7rhGiD36/9affffb4ec5c115b8f742cd34b663cff/best_to_do_apps.jpg?w=1520&fm=jpg&q=30&fit=thumb&h=760",
+    github:"https://github.com/0ALEX-2/todo-assignment",
+    deployed:"https://peaceful-fenglisu-8fbd55.netlify.app/",
+    title:"Full-stack MERN Todo Web App",
+    about:"This is a full-stack MERN web app. The frontend is made up of ReactJS and for styling I used Tailwind CSS. And for backend I used NodeJS, ExpressJS and MongoDB database. I Deployed the app on Netlify for frontend and Render.com for backend.",
+    techUsed:[
+      "https://w7.pngwing.com/pngs/201/90/png-transparent-logo-html-html5.png",
+      "https://w7.pngwing.com/pngs/696/424/png-transparent-logo-css-css3-thumbnail.png",
+      "https://w7.pngwing.com/pngs/241/797/png-transparent-cascading-style-sheets-css3-javascript-logo-world-wide-web-blue-angle-text.png",
+      "https://seeklogo.com/images/R/react-logo-7B3CE81517-seeklogo.com.png",
+      "https://w7.pngwing.com/pngs/293/485/png-transparent-tailwind-css-hd-logo-thumbnail.png",
+      "https://w7.pngwing.com/pngs/232/470/png-transparent-circle-js-node-node-js-programming-round-icon-popular-services-brands-vol-icon-thumbnail.png",
+      "https://w7.pngwing.com/pngs/925/447/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo-thumbnail.png",
+      "https://w7.pngwing.com/pngs/956/695/png-transparent-mongodb-original-wordmark-logo-icon-thumbnail.png"
+    ]
+  },
  
 ]
 
+
+const Work = () => {
+
+const [data,setData]=useState(initialData)
+let groupProjects=[]
+let ownProjects=[]
+
+initialData.map((ele)=>{
+  if(ele.type==="collab"){
+    groupProjects.push(ele)
+  }else if(ele.type==="single"){
+    ownProjects.push(ele)
+  }
+})
+
+const handleGroupProjects=()=>{
+setData(groupProjects)
+}
+const handleOwnProjects=()=>{
+setData(ownProjects)
+}
  
   return (
     <div className=' pt-[100px] md:mt-0 flex flex-col text-center px-10 justify-evenly mx-auto items-center mb-[100]'>
       
       <p className='uppercase tracking-[20px]  text-2xl text-slate-400'>projects</p>
  
+          <div className='flex justify-evenly gap-2 lg:w-[60%] mt-8 '>
+
+           <div><button className='rounded-full bg-blue-500 px-4 text-white font-semibold hover:bg-blue-700' onClick={()=>setData(initialData)}>ALL PROJECTS</button></div>
+           <div><button className='rounded-full bg-blue-500 px-4 text-white font-semibold hover:bg-blue-700' onClick={handleGroupProjects}>GROUP PROJECTS</button></div>
+           <div><button className='rounded-full bg-blue-500 px-4 text-white font-semibold hover:bg-blue-700' onClick={handleOwnProjects}>MY OWN PROJECTS</button></div>
+          
+          </div>
+
       
     <motion.div 
      initial={{
